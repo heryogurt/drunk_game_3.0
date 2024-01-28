e48e1cfa1a2e4eb4e2728e34f39cadb8cf95942d
polinka <23.krecu.p@gmail.com>
2024-01-28 21:26:12 +0300

    Initial Commit

---
+++ drunk_game_3.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  drunk_game_3.0
+//
+//  Created by Полина on 28.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ drunk_game_3.0/drunk_game_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  drunk_game_3_0App.swift
+//  drunk_game_3.0
+//
+//  Created by Полина on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct drunk_game_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
